04be7d28df5996bb2494d83f13ca497e70f143e5
streethacker <silverbullet7714@gmail.com>
2017-04-11 22:15:49 +0800

    :sparkless: add new confusing elements, draw line above text

--- pysvm/exc.py
+++ pysvm/exc.py
@@ -3,21 +3,7 @@
 __all__ = ['VerifyCodeError']
 
 
-from httplib import OK
-
-MSG_MAP = {
-    2101: 'Verify code not match',
-    2102: 'Verify code storage error',
-    2103: 'Trials more than allowed',
-}
-
-
 class VerifyCodeError(Exception):
-    status_code = OK
 
-    def __init__(self, error_code, error_msg=None, status_code=None):
-        self.error_code = error_code
-        self.error_msg = error_msg or MSG_MAP.get(error_code)
-        if status_code is not None:
-            self.status_code = status_code
-        super(VerifyCodeError, self).__init__(self.error_msg)
+    def __init__(self, message=None):
+        super(VerifyCodeError, self).__init__(message)
